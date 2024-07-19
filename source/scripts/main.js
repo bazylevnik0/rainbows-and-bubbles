a3270f5cbeafc286014b5901bbe99e4117a476cb
bazylevnik0 <bazylevnik0@gmail.com>
2024-07-19 21:34:19 +0300

    fixing logo screen

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -8,17 +8,33 @@ let image_logo = new Image();
     image_logo.src = "./data/sprites/logo.svg";
 
 let check_click_to_start = false;
+let parameter_logo_x = 0;
+let parameter_logo_y = 0;
+
+canvas.addEventListener("mousemove", (event) => {
+    console.log(event.clientX-canvas.offsetLeft);
+    if(event.clientX-canvas.offsetLeft>400){
+           parameter_logo_x -= 1;
+    } else parameter_logo_x += 1;
+    if(event.clientY-canvas.offsetTop>300){
+        parameter_logo_y -= 1;
+ } else parameter_logo_y += 1;
+    if (parameter_logo_x >  10) parameter_logo_x =  10;
+    if (parameter_logo_x < -10) parameter_logo_x = -10;
+    if (parameter_logo_y >  5) parameter_logo_y =  5;
+    if (parameter_logo_y < -5) parameter_logo_y = -5;
+});
+let i = 0;
 image_logo.onload = function(){
      
      let interval_click_to_start = setInterval( ()=>{
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, 800, 600);
-        ctx.drawImage(image_logo, 0, 0, 800, 600);
-        if (check_click_to_start) {
-            ctx.drawImage(image_click_to_start, 325, 500, 150, 20);
-            check_click_to_start = false;
-        } else {
-            check_click_to_start = true;        
+        ctx.drawImage(image_logo, 0 + parameter_logo_x, 0  + parameter_logo_y, 800, 600);
+        if (i > 0 && i < 5) {
+                ctx.drawImage(image_click_to_start, 325, 500, 150, 20);
         }
-     },500);
+        i++;
+        if (i > 10) i = 0;
+     },100);
 };
