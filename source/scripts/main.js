f566101f70d1b2c02e2d8ee0cdfc56d3d1fb715e
bazylevnik0 <bazylevnik0@gmail.com>
2024-07-20 12:01:41 +0300

    rewrite gamescene class

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -2,13 +2,55 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 class GameScene {
-    constructor (name) {
+    constructor (name, sprites) {
         this.name = name;
+        this.sprites = sprites;
     }
-    load   () {}
+    load   () {
+        for (let sprite in this.sprites) {
+            if (sprite.image) { // if it is a regular sprite
+                sprite.image.onload = ()=> { // if loaded
+                    sprite.loaded = true;    // set "the checker" as loaded
+                }
+            } else if (Array.isArray(sprite)) { // if it array of sprites
+                for (let i = 0; i < sprite.length; i++) {
+                    sprite[i].image.onload = ()=> { // if loaded
+                        sprite.loaded = true;       // set "the checker" as loaded
+                    }
+                }
+            }
+          }
+        
+          // Check all sprites - are they loaded
+          this.sprites.interval_load = setInterval(()=>{
+            let check = true;
+            for (let sprite in this.sprites) {
+                if (sprite.image) { // if regular sprite
+                    if (sprite.loaded == false) check = false;
+                } else if (Array.isArray(sprite)) { // if 
+                    for (let i = 0; i < sprite.length; i++) {
+                        if (sprite[i].loaded == false) check = false;
+                    }
+                }
+            } this.sprites.loaded = check;
+        
+            // If they loaded, finish the loading and start drawing the game
+            if (this.sprites.loaded == true) {
+                this.draw();
+               clearInterval(this.sprites.interval_load);
+            }
+          },10); 
+        
+        // Attach control to the scene
+        canvas.addEventListener("mousemove", this.control_mousemove);
+    }
     draw   () {}
-    play   () {}
-    unload () {}
+    control_mousemove (event) {
+        console.log("mousemove");
+    }
+    unload () {
+        canvas.removeEventListener("mousemove", control_mousemove);
+    }
 }
 
 class SpriteImage {
@@ -20,58 +62,23 @@ class SpriteImage {
     }
 }
 
-let scenes = {};
-    scenes["start_screen"] = new GameScene("start_screen");
 
 // Create the common object for sprites
-scenes.start_screen.sprites = {};
-scenes.start_screen.sprites.loaded = false; // when all sprites inside loaded - it true
+let sprites = {};
+sprites.loaded = false; // when all sprites inside loaded - it true
 
 // Create sprites images (also trigger onload in the image object)
-scenes.start_screen.sprites.click_to_start = new SpriteImage("./data/sprites/click-to-start.svg");
-scenes.start_screen.sprites.logo           = new SpriteImage("./data/sprites/logo.svg"); 
-scenes.start_screen.sprites.background     = [];
-scenes.start_screen.sprites.background.timer = 0; // in array possible to set the common parameter as in object
+sprites.click_to_start = new SpriteImage("./data/sprites/click-to-start.svg");
+sprites.logo           = new SpriteImage("./data/sprites/logo.svg"); 
+sprites.background     = [];
+sprites.background.timer = 0; // in array possible to set the common parameter as in object
 for (let i = 0; i < 5; i++) {
-    scenes.start_screen.sprites.background[i] = new SpriteImage("./data/sprites/background" + i + ".svg");
+    sprites.background[i] = new SpriteImage("./data/sprites/background" + i + ".svg");
 }
 
-// Loading sprites and the game function
-scenes.start_screen.load = function () {
-  for (let sprite in scenes.start_screen.sprites) {
-    if (sprite.image) { // if it is a regular sprite
-        sprite.image.onload = ()=> { // if loaded
-            sprite.loaded = true;    // set "the checker" as loaded
-        }
-    } else if (Array.isArray(sprite)) { // if it array of sprites
-        for (let i = 0; i < sprite.length; i++) {
-            sprite[i].image.onload = ()=> { // if loaded
-                sprite.loaded = true;       // set "the checker" as loaded
-            }
-        }
-    }
-  }
 
-  // Check all sprites - are they loaded
-  scenes.start_screen.sprites.interval_load = setInterval(()=>{
-    let check = true;
-    for (let sprite in scenes.start_screen.sprites) {
-        if (sprite.image) { // if regular sprite
-            if (sprite.loaded == false) check = false;
-        } else if (Array.isArray(sprite)) { // if 
-            for (let i = 0; i < sprite.length; i++) {
-                if (sprite[i].loaded == false) check = false;
-            }
-        }
-    } scenes.start_screen.sprites.loaded = check;
-
-    // If they loaded, finish the loading and start drawing the game
-    if (scenes.start_screen.sprites.loaded == true) {
-        scenes.start_screen.draw();
-       clearInterval(scenes.start_screen.sprites.interval_load);
-    }
-  },10);
-}
+let scenes = {};
+    scenes["start_screen"] = new GameScene("start_screen", sprites);
 
 // Draw sprites and the game
 scenes.start_screen.draw = function () {
@@ -89,19 +96,19 @@ scenes.start_screen.draw = function () {
         if (scenes.start_screen.sprites.background.timer > 4) scenes.start_screen.sprites.background.timer = 0;
      },100);
 };
-scenes.start_screen.load();
 
-// Control the game
-canvas.addEventListener("mousemove", (event) => {
+
+scenes.start_screen.control_mousemove = function (event) {
     if (event.clientX-canvas.offsetLeft > 400) {
         scenes.start_screen.sprites.logo.shift.x -= 1;
     } else scenes.start_screen.sprites.logo.shift.x += 1;
     if (event.clientY-canvas.offsetTop > 300) {
         scenes.start_screen.sprites.logo.shift.y -= 1;
- } else scenes.start_screen.sprites.logo.shift.y += 1;
+} else scenes.start_screen.sprites.logo.shift.y += 1;
     if (scenes.start_screen.sprites.logo.shift.x >  10) scenes.start_screen.sprites.logo.shift.x =  10;
     if (scenes.start_screen.sprites.logo.shift.x < -10) scenes.start_screen.sprites.logo.shift.x = -10;
     if (scenes.start_screen.sprites.logo.shift.y >  5)  scenes.start_screen.sprites.logo.shift.y =  5;
     if (scenes.start_screen.sprites.logo.shift.y < -5)  scenes.start_screen.sprites.logo.shift.y = -5;
-});
+}
 
+scenes.start_screen.load();
